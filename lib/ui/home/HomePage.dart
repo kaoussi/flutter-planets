6857a87ce08b56a4b682f30697c5cdaccb42a149
Kaoussi Souhail <kaoussi.souhail@gmail.com>
2018-03-07 09:48:55 +0000

    first commit

---
+++ lib/ui/home/HomePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:planets/ui/home/GradientAppBar.dart';
+import 'package:planets/ui/home/HomePageBody.dart';
+
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new Column(
+        children: <Widget>[
+          new GradientAppBar("treva"),
+          new HomePageBody(),
+        ],
+      ),
+    );
+  }
+}
